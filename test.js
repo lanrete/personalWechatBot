deec6419d4e690f8c28b45cd284eeed57a8c0134
Yifan Li <jeorito@gmail.com>
2019-04-27 01:42:19 +0800

    remove un-needed node-module, should use setTimeout and setInterval instead

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var tools = require('./tools')
+var tools = require("./tools");
+var schedule = require("node-schedule");
+var moment = require("moment");
 
-var message = tools.remind("5min", "testing")
-console.log(message)
+var message = tools.remind("1min", "testing");
+console.log(message.message);
+
+// TODO Need to change the console log messages, is putting too much information in it
+setTimeout(
+  _ => console.log(`Reminding about ${message.message}`),
+  message.moment.diff(moment())
+);
--- tools.js
+++ tools.js
@@ -6,21 +6,38 @@ var moment = require("moment");
  */
 function remind(time, message) {
   var messageText = `无法解析${time}`;
+  var isSuccess = false;
   if (time.includes("min")) {
     let number_part = Number(time.replace("min", ""));
     var remindTime = moment().add(number_part, "minutes");
-    messageText = 
-      `Will remind you about [${message}] at ${remindTime.format("DD-MMM HH:mm")}`
-  } else if (time.includes("hour")) {
+    messageText = `Will remind you about [${message}] at ${remindTime.format(
+      "DD-MMM HH:mm"
+    )}`;
+    isSuccess = true;
+    return {
+      message: messageText,
+      isSuccess: isSuccess,
+      moment: remindTime
+    };
+  }
+  if (time.includes("hour")) {
     let number_part = Number(time.replace("hour", ""));
     var remindTime = moment().add(number_part, "hour");
-    messageText = 
-      `Will remind you about ${message} at ${remindTime.format("DD-MMM HH:mm")}`
-  } 
-  return messageText
+    messageText = `Will remind you about ${message} at ${remindTime.format(
+      "DD-MMM HH:mm"
+    )}`;
+    return {
+      message: messageText,
+      isSuccess: isSuccess,
+      moment: remindTime
+    };
+  }
+  return {
+    message: messageText,
+    isSuccess: isSuccess
+  };
 }
 
-
 module.exports = {
-    remind: remind
+  remind: remind
 };
