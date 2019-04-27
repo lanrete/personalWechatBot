06c7bbd7c3a85d1e3faf5615af125fe2e7aa51c2
Yifan Li <jeorito@gmail.com>
2019-04-27 21:19:58 +0800

    removing unneeded files

--- test.js
+++
@@ -1,9 +0,0 @@
-var tools = require("./tools");
-var moment = require("moment");
-
-var message = tools.parseRemindText("!remind me 2min 吃饭")
-
-setTimeout(
-  _ => console.log(message.reminder),
-  message.moment.diff(moment())
-);
